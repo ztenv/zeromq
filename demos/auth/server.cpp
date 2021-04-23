74e4d429eb4bd130a5eaad0c991327277bb351dd
ztenv <class7class@163.com>
2021-04-23 14:55:45 +0000

    feat(auth): modify ZMQ_REP to ZMQ_ROUTER

--- demos/auth/server.cpp
+++ demos/auth/server.cpp
@@ -17,7 +17,7 @@ using namespace std;
 
 void auth_server(void *context);
 
-void build_reply(std::vector<char*> msg, void *socket);
+void build_reply(std::vector<char*> msg, void *socket, const char * status_code = "200", const char * status_text = "ok");
 
 int main()
 {
@@ -52,7 +52,7 @@ int main()
 
 void auth_server(void *context) {
     
-    void * auth_socket = zmq_socket(context, ZMQ_REP);
+    void * auth_socket = zmq_socket(context, ZMQ_ROUTER);
 
     auto rc = zmq_bind(auth_socket, "inproc://zeromq.zap.01");
     cout << "auth_socket zmq_bind:" << rc << endl;
@@ -80,28 +80,28 @@ void auth_server(void *context) {
         } else {
             cout << "frame size:" << msg.size() << endl;
 
-            char * version = msg[0];
+            char * version = msg[2];
             cout << "version:" << version << endl;
 
-            char * sequence = msg[1];
+            char * sequence = msg[3];
             cout << "sequence:" << sequence << endl;
 
-            char * domain = msg[2];
+            char * domain = msg[4];
             cout << "domain:" << domain << endl;
 
-            char * address = msg[3];
+            char * address = msg[5];
             cout << "address:" << address << endl;
 
-            char * identity = msg[4];
+            char * identity = msg[6];
             cout << "identity:" << identity << endl;
 
-            char * mechanism = msg[5];
+            char * mechanism = msg[7];
             cout << "mechanism:" << mechanism << endl;
 
-            char * user = msg[6];
+            char * user = msg[8];
             cout << "user:" << user << endl;
 
-            char * pwd = msg[7];
+            char * pwd = msg[9];
             cout << "pwd:" << pwd << endl;
 
             build_reply(msg, auth_socket);
@@ -112,50 +112,53 @@ void auth_server(void *context) {
     }
 }
 
-void build_reply(std::vector<char*> msg, void *socket) {
-    //zmq_msg_t p1;
-    //int rc = zmq_msg_init_size(&p1, 5);
-    //memcpy(zmq_msg_data(&p1), msg[0], 5);
+void build_reply(std::vector<char*> msg, void *socket, const char * status_code, const char * status_text) {
+    zmq_msg_t p0;
+    int rc = zmq_msg_init_size(&p0, 5);
+    memcpy(zmq_msg_data(&p0), msg[0], 5);       
+    rc = zmq_msg_send(&p0, socket, ZMQ_SNDMORE);  // the dealer id
+    cout << "send res:" << rc << endl;
 
-    //rc = zmq_msg_send(&p1, socket, ZMQ_SNDMORE);
-
-    int rc = 0;
+    zmq_msg_t p1;
+    rc = zmq_msg_init_size(&p1, 0);
+    rc = zmq_msg_send(&p1, socket, ZMQ_SNDMORE);  //zero frame
+    cout << "send res:" << rc << endl;
 
     zmq_msg_t p2;
-    rc = zmq_msg_init_size(&p2, strlen(msg[0]));
-    memcpy(zmq_msg_data(&p2), msg[0], strlen(msg[0]));
+    rc = zmq_msg_init_size(&p2, strlen(msg[2]));
+    memcpy(zmq_msg_data(&p2), msg[2], strlen(msg[2])); // version
     rc = zmq_msg_send(&p2, socket, ZMQ_SNDMORE);
     cout << "send res:" << rc << endl;
 
     zmq_msg_t p3;
-    rc = zmq_msg_init_size(&p3, strlen(msg[1]));
-    memcpy(zmq_msg_data(&p3), msg[1], strlen(msg[1]));
+    rc = zmq_msg_init_size(&p3, strlen(msg[3]));
+    memcpy(zmq_msg_data(&p3), msg[3], strlen(msg[3])); //sequence
     rc = zmq_msg_send(&p3, socket, ZMQ_SNDMORE);
     cout << "send res:" << rc << endl;
 
     zmq_msg_t p4;
-    const char * status_code = "200";
+    //const char * status_code = "200";
     rc = zmq_msg_init_size(&p4, strlen(status_code));
-    memcpy(zmq_msg_data(&p4), status_code, strlen(status_code));
+    memcpy(zmq_msg_data(&p4), status_code, strlen(status_code)); //status_code
     rc = zmq_msg_send(&p4, socket, ZMQ_SNDMORE);
     cout << "send res:" << rc << endl;
 
     zmq_msg_t p5;
-    const char * status_text = "ok";
+    //const char * status_text = "ok";
     rc = zmq_msg_init_size(&p5, strlen(status_text));
-    memcpy(zmq_msg_data(&p5), status_code, strlen(status_text));
+    memcpy(zmq_msg_data(&p5), status_code, strlen(status_text)); //status_text
     rc = zmq_msg_send(&p5, socket, ZMQ_SNDMORE);
     cout << "send res:" << rc << endl;
 
     zmq_msg_t p6;
-    rc = zmq_msg_init_size(&p6, strlen(msg[6]));
-    memcpy(zmq_msg_data(&p6), msg[6], strlen(msg[6]));
+    rc = zmq_msg_init_size(&p6, strlen(msg[8]));
+    memcpy(zmq_msg_data(&p6), msg[8], strlen(msg[8])); //user_id/user_name
     rc = zmq_msg_send(&p6, socket, ZMQ_SNDMORE);
     cout << "send res:" << rc << endl;
 
     zmq_msg_t p7;
     rc = zmq_msg_init_size(&p7, 0);
-    rc = zmq_msg_send(&p7, socket, 0);
+    rc = zmq_msg_send(&p7, socket, 0);  //zero frame
     cout << "send res:" << rc << endl;
 
 }
